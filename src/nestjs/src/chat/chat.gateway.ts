2dc423f34136e58d41c38cf85c9ae111945e371e
Winspear22 <adnen.aloui2@hotmail.com>
2023-08-09 15:54:04 +0200

    Ajout de la gestion des tokens exprirés, mal formés, vides, faux et si on met un refresh token à la place

--- src/nestjs/src/chat/chat.gateway.ts
+++ src/nestjs/src/chat/chat.gateway.ts
@@ -8,13 +8,17 @@ import { Socket, Server } from 'socket.io';
 import { UserService } from 'src/user/user.service';
 import * as colors from '../colors';
 import { JwtService } from '@nestjs/jwt';
+import { decode, TokenExpiredError } from 'jsonwebtoken';  // Ne pas oublier d'importer les méthodes nécessaires
 
 export interface JwtPayload {
   username: string;
   auth: boolean;
+  sub?: string;
+  iat?: number;
+  exp?: number;
 }
 
-@WebSocketGateway({cors: true})
+@WebSocketGateway({cors: true, namespace: 'chats'})
 export class ChatGateway 
 {
   constructor(private userService: UserService,
@@ -30,38 +34,61 @@ export class ChatGateway
     this.server.emit('message', msg);
   }
 
-  @SubscribeMessage('Connection')
-  async handleConnection(@ConnectedSocket() client: Socket) 
-  {
+@SubscribeMessage('Connection')
+async handleConnection(@ConnectedSocket() client: Socket) {
     const accessTokenCookie = client.handshake.headers.cookie;
     console.log("User connected : ", colors.WHITE, client.id, " connection status : ", colors.GREEN, client.connected, colors.RESET);
 
     if (!accessTokenCookie) {
-      console.log('Access Token Cookie is missing.');
-      return this.handleDisconnect(client);
+        console.log('Access Token Cookie is missing.');
+        return this.handleDisconnect(client);
     }
-    // Faites ce que vous avez besoin de faire avec le jeton d'authentification.
-      console.log('Access Token Cookie:', accessTokenCookie);
-      const decodedCookie = decodeURIComponent(accessTokenCookie); // Décodez le cookie
-      const userData = JSON.parse(decodedCookie);
-      const { username } = userData;
-      const { refreshToken } = userData;
-      const { accessToken } = userData;
-      console.log(username);
-      console.log(refreshToken);
-      console.log(accessToken);
-      const payload: JwtPayload = await this.jwtService.verifyAsync(accessToken, {
-        secret: process.env.ACCESS_TOKEN});
-      //const payload: JwtPayload = this.jwtService.verify(accessToken, {secret: process.env.JWT_ACCESS_TOKEN_SECRET});
-      console.log("payload === ", payload);
-      const user = await this.userService.findUserByUsername(username);
-      client.data.user = user;
-      console.log(client.data.user);
-      if (user)
+
+    console.log('Access Token Cookie:', accessTokenCookie);
+    const decodedCookie = decodeURIComponent(accessTokenCookie); 
+    const userData = JSON.parse(decodedCookie);
+    const { username, refreshToken, accessToken } = userData;
+
+    const decodedPayload = decode(accessToken) as JwtPayload;
+    if (!decodedPayload) {
+        console.log('Token is invalid or malformed.');
+        return this.handleDisconnect(client);
+    }
+
+    const currentTime = Math.floor(Date.now() / 1000);
+    console.log("EXP === ",  decodedPayload.exp);
+    if (decodedPayload.exp && decodedPayload.exp < currentTime) {
+        console.log('Token has expired.');
+        return this.handleDisconnect(client);
+    }
+
+    let payload: JwtPayload;
+    try {
+        payload = await this.jwtService.verifyAsync(accessToken, {
+            secret: process.env.ACCESS_TOKEN
+        });
+    } catch (error) {
+        if (error instanceof TokenExpiredError) {
+            console.log('Token has expired.');
+            return this.handleDisconnect(client);
+        } else {
+            console.log('Token is invalid:', error.message);
+            return this.handleDisconnect(client);
+        }
+    }
+
+    console.log("payload === ", payload);
+
+    const user = await this.userService.findUserByUsername(username);
+    client.data.user = user;
+    console.log(client.data.user);
+
+    if (user)
         return user;
-      else
+    else
         return this.handleDisconnect(client); 
-  }
+}
+
 
   handleDisconnect(client: Socket)
   {
