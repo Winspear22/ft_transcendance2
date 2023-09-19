5e2ca44ee9926b4961b438bb1b9dc85250819642
Administrateur <maeva.albrand@icloud.com>
2023-09-19 22:18:37 +0200

    Acception/Refus de emitFriendRequest

--- src/vuejs/src/chat/friendRequestItem.vue
+++ src/vuejs/src/chat/friendRequestItem.vue
@@ -2,22 +2,47 @@
     <div class="friend-request-item">
       <img :src="request.profile_picture" alt="Profile" />
       <span>{{ request.username }}</span>
+      <button @click="acceptRequest">Accepter</button>
+      <button @click="declineRequest">Refuser</button>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'FriendRequestItem',
-    props: {
-      request: {
-        type: Object,
-        required: true
-      }
+</template>
+
+<script>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+export default {
+  name: 'FriendRequestItem',
+  props: {
+    request: {
+      type: Object,
+      required: true
     }
-  };
-  </script>
-  
-  <style scoped>
+  },
+  setup(props) {
+    const store = useStore();
+    const socket = store.getters.socket;
+
+    // En utilisant computed pour garder le username de la demande
+    const senderUsername = computed(() => props.request.username);
+
+    const acceptRequest = () => {
+      socket.emit('acceptFriendRequest', { receiverUsername: senderUsername.value });
+    };
+
+    const declineRequest = () => {
+      socket.emit('refuseFriendRequest', { receiverUsername: senderUsername.value });
+    };
+
+    return {
+      acceptRequest,
+      declineRequest
+    };
+  }
+};
+</script>
+
+<style scoped>
   .friend-request-item {
     display: flex;
     align-items: center;
