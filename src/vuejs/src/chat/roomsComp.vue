1e1b6d965c680ed1fbe9795a0bb0052cf8884023
Administrateur <maeva.albrand@icloud.com>
2023-10-02 18:40:51 +0200

    correction des doubles msg dans le chat

--- src/vuejs/src/chat/roomsComp.vue
+++ src/vuejs/src/chat/roomsComp.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="dm-container">
-    <!-- Liste des rooms -->
     <div v-if="rooms.length > 0" class="rooms-list">
       <div v-for="room in rooms" :key="room.id" 
            @click="selectRoom(room)" 
@@ -8,11 +7,7 @@
         <span class="chat-name">{{ room.roomName }}</span>
       </div>
     </div>
-
-    <!-- Séparateur vertical -->
     <div class="vertical-separator"></div>
-
-    <!-- Fenêtre de chat sélectionnée -->
     <div v-if="selectedRoom" class="selected-chat">
       <h3>{{ selectedRoom.roomName }}</h3>
       <ul>
@@ -20,7 +15,6 @@
           {{ findUserNameById(message.senderId, selectedRoom.userDTOs) }}: {{ message.text }}
         </li>
       </ul>
-
       <send-chat :selected-room="selectedRoom"></send-chat>
     </div>
   </div>
@@ -31,102 +25,64 @@ import SendChat from './sendChat.vue';
 
 export default {
   components: { SendChat },
-  
   data() {
     return {
       rooms: [],
       selectedRoom: null,
-      processedMessageIds: [] // Ajouté
+      processedMessageIds: []
     };
   },
-
   computed: {
     socketChat() {
       return this.$store.getters.socketChat;
     }
   },
-
   methods: {
     selectRoom(room) {
       this.selectedRoom = room;
     },
-
     findUserNameById(userId, users) {
       const user = users.find(u => u.id === userId);
       return user ? user.username : 'Unknown User';
     },
-
     handleNewMessage(newMessage, senderDetails) {
-      // Vérifiez si le message a déjà été traité
       if (this.processedMessageIds.includes(newMessage.id)) return;
-
-      // Ajoutez l'ID du message au tableau des messages traités
       this.processedMessageIds.push(newMessage.id);
-
       const room = this.rooms.find(r => r.roomName === newMessage.room.roomName);
       if (room) {
         newMessage.senderUsername = senderDetails.senderUsername;
         room.messages.push(newMessage);
       }
+    },
+    attachSocketListeners() {
+      this.socketChat.on('emitRooms', this.updateRooms);
+      this.socketChat.on('sendMessage', this.receiveMessage);
+    },
+    detachSocketListeners() {
+      this.socketChat.off('emitRooms', this.updateRooms);
+      this.socketChat.off('sendMessage', this.receiveMessage);
+    },
+    updateRooms(rooms) {
+      this.rooms = rooms;
+    },
+    receiveMessage(newMessage, senderDetails) {
+      this.handleNewMessage(newMessage, senderDetails);
     }
   },
-
   created() {
-    this.socketChat.emit('Connection');
-
-    if (this.socketChat) {
-      this.socketChat.on('emitRooms', (rooms) => {
-        this.rooms = rooms;
-      });
-
-      this.socketChat.on('sendMessage', (newMessage, senderDetails) => {
-        this.handleNewMessage(newMessage, senderDetails);
-      });
-    } else {
+    if (!this.socketChat) {
       console.error("Socket Chat non initialisé!");
+      return;
     }
+    this.socketChat.emit('Connection');
+    this.attachSocketListeners();
   },
-
   beforeDestroy() {
-    if (this.socketChat) {
-      this.socketChat.off('emitRooms');
-      this.socketChat.off('sendMessage');
-    }
+    this.detachSocketListeners();
   }
 };
 </script>
 
 <style scoped>
-.dm-container {
-  display: flex;
-}
-
-.rooms-list {
-  width: 250px;
-  overflow-y: auto;
-  border-right: 1px solid #2fe8ee;
-}
-
-.vertical-separator {
-  width: 1px;
-  background-color: #2fe8ee;
-  height: 100%;
-}
-
-.selected-chat {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-}
-
-.chat-name {
-  color: #2fe8ee;
-  cursor: pointer;
-  transition: color 0.3s;
-}
-
-.chat-name:hover {
-  color: black;
-}
+/* ... (Votre code CSS actuel) */
 </style>
--- src/vuejs/src/chat/sendChat.vue
+++ src/vuejs/src/chat/sendChat.vue
@@ -1,43 +1,49 @@
 <template>
-    <div>
-      <info-user @userInfoFetched="updateSenderUsername"></info-user>
-      <textarea v-model="messageContent"></textarea>
-      <button @click="sendMessage">Envoyer</button>
-    </div>
-  </template>
-  
-  <script>
-  import InfoUser from '../setting/infoUser';
-  
-  export default {
-    components: {
-      InfoUser,
-    },
-    props: ['selectedRoom'],
-    data() {
-      return {
-        messageContent: "",
-        senderUsername: null
-      };
-    },
-    methods: {
-      sendMessage() {
-        if (!this.senderUsername) {
-          console.error("Nom d'utilisateur non défini. Ne peut pas envoyer le message.");
-          return;
-        }
-  
-        this.$store.getters.socketChat.emit('sendMessage', {
-          channelName: this.selectedRoom.roomName,
-          senderUsername: this.senderUsername,
-          message: this.messageContent
-        });
-        this.messageContent = "";
-      },
-      updateSenderUsername(userInfo) {
-        this.senderUsername = userInfo.username;
+  <div>
+    <info-user @userInfoFetched="updateSenderUsername"></info-user>
+    <textarea v-model="messageContent"></textarea>
+    <button @click="sendMessage">Envoyer</button>
+  </div>
+</template>
+
+<script>
+import InfoUser from '../setting/infoUser';
+
+export default {
+  components: { InfoUser },
+  props: ['selectedRoom'],
+  data() {
+    return {
+      messageContent: "",
+      senderUsername: null
+    };
+  },
+  methods: {
+    sendMessage() {
+      if (!this.senderUsername) {
+        console.error("Nom d'utilisateur non défini. Ne peut pas envoyer le message.");
+        return;
       }
+      this.$store.getters.socketChat.emit('sendMessage', {
+        channelName: this.selectedRoom.roomName,
+        senderUsername: this.senderUsername,
+        message: this.messageContent
+      });
+      this.messageContent = "";
+    },
+    updateSenderUsername(userInfo) {
+      this.senderUsername = userInfo.username;
+    }
+  },
+  watch: {
+    selectedRoom: {
+      handler(newVal, oldVal) {
+        if (newVal && newVal !== oldVal) {
+          this.messageContent = "";
+        }
+      },
+      deep: true
     }
   }
-  </script>
-  
+};
+</script>
